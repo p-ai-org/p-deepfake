078fda6eb04fa1f1ba20c4e9e0525e903ecda730
eshaan <eshaan100102@gmail.com>
2021-11-07 14:20:00 -0800

    Accounted for error in algo naming

--- region_detection.py
+++ region_detection.py
@@ -11,23 +11,30 @@ import os
 def analyze_regions(regions, algo):
   keypoints_by_region = []
   for region in regions:
+    # SIFT
     if (algo == "SIFT"):
       gray = cv2.cvtColor(region,cv2.COLOR_BGR2GRAY)
       sift = cv2.xfeatures2d.SIFT_create()
       kp = sift.detect(gray,None)
       keypoints_by_region.append(kp) 
+    # ORB
     else if (algo == "ORB"):
       orb = cv2.ORB_create()
       # find the keypoints and feature descriptors with ORB
       kp = orb.detect(img,None)
       keypoints_by_region.append(kp) 
+    # FAST 
     else if (algo == "FAST"):
       fast = cv2.FastFeatureDetector_create()
 	    kp = fast.detect(img, None)
       keypoints_by_region.append(kp)
-    else:
+    # SURF 
+    else if (algo == "SURF"):
       surf = cv2.xfeatures2d.SURF_create(400)
       kp = surf.detect(region,None)
       keypoints_by_region.append(kp) 
+    else:
+      print("Invalid algo")
+      raise
     
   return keypoints_by_region
