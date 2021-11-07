6f90a5bc7e1225d1f5c22c65611b5bf6a399b780
eshaan <eshaan100102@gmail.com>
2021-11-07 14:16:18 -0800

    Added FAST and ORB implementation

--- region_detection.py
+++ region_detection.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 
 import os
 
-# SIFT and SURF implementation
+# peforms given algo on region 
 def analyze_regions(regions, algo):
   keypoints_by_region = []
   for region in regions:
@@ -16,6 +16,15 @@ def analyze_regions(regions, algo):
       sift = cv2.xfeatures2d.SIFT_create()
       kp = sift.detect(gray,None)
       keypoints_by_region.append(kp) 
+    else if (algo == "ORB"):
+      orb = cv2.ORB_create()
+      # find the keypoints and feature descriptors with ORB
+      kp = orb.detect(img,None)
+      keypoints_by_region.append(kp) 
+    else if (algo == "FAST"):
+      fast = cv2.FastFeatureDetector_create()
+	    kp = fast.detect(img, None)
+      keypoints_by_region.append(kp)
     else:
       surf = cv2.xfeatures2d.SURF_create(400)
       kp = surf.detect(region,None)
